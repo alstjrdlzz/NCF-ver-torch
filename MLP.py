39a0fceb5edf2c1099d7fcdb8fe17d209256a20b
alstjrdlzz <tjalstjr95@gmail.com>
2023-03-31 01:02:53 +0900

    feat: MLP Layers

--- MLP.py
+++ MLP.py
@@ -1,9 +1,26 @@
+from typing import List
+
 import torch
 import torch.nn as nn
 
 
 class MLPLayers(nn.Module):
-    def __init__(self):
-        pass
+    """
+    keras는 dense(layer[0]) + dense(layer[1]) + dense(layer[2]) + ... 이런식으로 구현돰
+    torch는 Linear(embe*2, layer[0]) + Linear(layer[0], layer[1]) + ... + Linear(layer[n-2], layer[n-1])
+    """
+    def __init__(self,
+                 layer: List):
+        
+        super().__init__()
+
+        self.layer = layer
+        for i in range(len(layer)):
+            self.mlp_layers.append(nn.Linear(layer[i], layer[]))
+        
+
+    def forward(self, x):
+
+        return
 
 
