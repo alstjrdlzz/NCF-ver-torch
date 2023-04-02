51f43b3cf73a5b861562255ff8318189b9b4e70b
alstjrdlzz <tjalstjr95@gmail.com>
2023-04-02 21:37:42 +0900

    feat: GMF 모듈 개발

---
+++ GMF.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+
+
+class GMF(nn.Module):
+    def __init__(self,
+                 N: int,
+                 M: int,
+                 K: int):
+        super().__init__()
+
+        # Layer
+        self.user_embedding_layer = nn.Embedding(M, K)
+        self.item_embedding_layer = nn.Embedding(N, K)
+        self.output_layer = nn.Linear(K, 1)
+        
+        
+    def forward(self, user, item):
+        user_embedding = self.user_embedding_layer(user)
+        item_embedding = self.item_embedding_layer(item)
+        x = user_embedding * item_embedding
+        x = self.output_layer(x)
+        output = nn.Sigmoid(x)
+        return output
