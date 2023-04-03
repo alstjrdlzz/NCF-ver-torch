0e67998d48e1f6e966010765a9bfe9196b9882a5
alstjrdlzz <tjalstjr95@gmail.com>
2023-04-04 00:00:01 +0900

    FEAT: dataset 모듈 개발

---
+++ data/dataset.py
@@ -0,0 +1,33 @@
+import torch
+from torch.utils.data import Dataset
+from pathlib import Path
+import pandas as pd
+
+
+class TrainDataset(Dataset):
+    def __init__(self):
+        pass
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self):
+        pass
+
+
+class ValidDataset(Dataset):
+    def __init__(self):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self):
+        pass
+
+
+class TestDataset(Dataset):
+    def __init__(self):
+        pass
+    def __len__(self):
+        pass
+    def __getitem__(self):
+        pass
