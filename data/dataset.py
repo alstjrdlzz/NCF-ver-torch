851cae3f1deaa6c4539da28443c1a3590c246cd1
alstjrdlzz <tjalstjr95@gmail.com>
2023-04-04 11:07:53 +0900

    FEAT: TranDataset 개발

--- data/dataset.py
+++ data/dataset.py
@@ -1,33 +1,22 @@
-import torch
-from torch.utils.data import Dataset
 from pathlib import Path
 import pandas as pd
+import torch
+from torch.utils.data import Dataset
+from preprocessing import preprocessor
 
 
 class TrainDataset(Dataset):
-    def __init__(self):
-        pass
+    def __init__(self,
+                 path: Path):
+        raw_df = pd.read_csv(path/'u.data', sep='\t', 
+                             encoding='latin-1', header=None)
+        raw_df.columns = ['user_id', 'movie_id', 'rating', 'timestamp']
+        self.implicit_df = preprocessor(raw_df)
 
     def __len__(self):
-        pass
+        return len(self.implicit_df)
 
-    def __getitem__(self):
-        pass
-
-
-class ValidDataset(Dataset):
-    def __init__(self):
-        pass
-    def __len__(self):
-        pass
-    def __getitem__(self):
-        pass
-
-
-class TestDataset(Dataset):
-    def __init__(self):
-        pass
-    def __len__(self):
-        pass
-    def __getitem__(self):
-        pass
+    def __getitem__(self, idx):
+        X = self.implicit_df[['user_id', 'movie_id']].values[idx, :]
+        y = self.implicit_df['implicit_feedback'].values[idx]
+        return torch.tensor(X), torch.tensor(y)
