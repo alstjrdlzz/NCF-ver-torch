5c8e04c9db79e00581ef95c70acccb386a531541
alstjrdlzz <tjalstjr95@gmail.com>
2023-04-08 01:32:07 +0900

    FIX: change argument

--- data/dataset.py
+++ data/dataset.py
@@ -1,22 +1,15 @@
-from pathlib import Path
-import pandas as pd
 import torch
 from torch.utils.data import Dataset
-from preprocessing import preprocessor
 
 
 class TrainDataset(Dataset):
-    def __init__(self,
-                 path: str):
-        self.path = Path(path)
-        raw_df = pd.read_csv(self.path/'u.data', sep='\t', encoding='latin-1', header=None)
-        raw_df.columns = ['user_id', 'movie_id', 'rating', 'timestamp']
-        self.implicit_df = preprocessor(raw_df)
+    def __init__(self, df):
+        self.df = df
 
     def __len__(self):
         return len(self.implicit_df)
 
     def __getitem__(self, idx):
-        X = self.implicit_df[['user_id', 'movie_id']].values[idx, :]
-        y = self.implicit_df['implicit_feedback'].values[idx]
+        X = self.df[['user_id', 'movie_id']].values[idx, :]
+        y = self.df['implicit_feedback'].values[idx]
         return torch.tensor(X), torch.tensor(y)
